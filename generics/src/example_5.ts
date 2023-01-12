384109245c9ef121bbeae2ce3213283b2d89d011
Hannachi Ramzi <ramzi.hannachi1991@gmail.com>
2023-01-12 13:16:16 +0100

    Generics - add example 5

---
+++ generics/src/example_5.ts
@@ -0,0 +1,21 @@
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string,): Partial<CourseGoal> {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  return courseGoal
+}
+
+console.log('example 5');
+console.log({ createCourseGoal: createCourseGoal('my title', 'my description') })
+console.log();
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu'); // => error
+// names.pop(); // => error
