e523c25fb93df586253445b9b92bac832adef411
Hannachi Ramzi <ramzi.hannachi1991@gmail.com>
2023-01-13 10:24:18 +0100

    Decorators - add example 4

---
+++ decorators/src/example_4.ts
@@ -0,0 +1,75 @@
+
+interface ValidatorConfig {
+    // ['required', 'positive']
+    [validatableProp: string]: string[];
+}
+
+let registeredValidators: ValidatorConfig = {};
+
+function Required(_target: any, propName: string) {
+    registeredValidators = {
+        ...registeredValidators,
+        [propName]: ['required']
+    };
+}
+
+function PositiveNumber(_target: any, propName: string) {
+    registeredValidators = {
+        ...registeredValidators,
+        [propName]: ['positive']
+    };
+}
+
+function validate<T extends Record<string, any>>(obj: T) {
+    if (!registeredValidators) {
+        return true;
+    }
+
+    let isValid = true;
+
+    for (const [prop, validators] of Object.entries(registeredValidators)) {
+        for (const validator of validators) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+     }
+
+    return isValid;
+}
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const titleEl = <HTMLInputElement>document.getElementById('title');
+    const priceEl = <HTMLInputElement>document.getElementById('price');
+
+    const title = titleEl.value;
+    const price = Number(priceEl.value);
+
+    const createdCourse = new Course(title, price);
+
+    if (!validate<Course>(createdCourse)) {
+        alert('Invalid input, please try again!');
+        return;
+    }
+    console.log({ createdCourse });
+});
