3f96df3412d70f768b48453887dbadbac20b386b
Hannachi Ramzi <ramzi.hannachi1991@gmail.com>
2023-01-20 12:39:59 +0100

    Refacto example-5

--- advanced-types/src/example-5/script.ts
+++ advanced-types/src/example-5/script.ts
@@ -27,7 +27,47 @@ function moveAnimal(animal: Animal) {
   console.log('Moving at speed: ' + speed);
 }
 
-console.log('Example 5')
+console.log('Example 5 (1)')
 moveAnimal({type: 'bird', flyingSpeed: 10});
 console.log()
 
+/**
+ * use class
+ */
+
+abstract class Animal1 {}
+
+class Bird1 extends Animal1{
+  // type: 'bird';
+  public flyingSpeed: number;
+  constructor(flyingSpeed: number) {
+    super();
+    this.flyingSpeed = flyingSpeed
+  }
+}
+
+class Horse1 extends Animal1{
+  // type: 'horse';
+  public runningSpeed: number;
+  constructor(runningSpeed: number) {
+    super();
+    this.runningSpeed = runningSpeed
+  }
+}
+
+
+function moveAnimal1(animal: Animal1) {
+  let speed;
+  if (animal instanceof Bird1) {
+    speed = animal.flyingSpeed
+  }
+  if (animal instanceof Horse1) {
+    speed = animal.runningSpeed
+  }
+  console.log('Moving at speed: ' + speed);
+}
+
+console.log('Example 5 (2)')
+const bird1 = new Bird1(10)
+moveAnimal1(bird1);
+console.log()
