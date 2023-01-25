f4758a277e77d858f52707d1ebb29079ba1b8599
Hannachi Ramzi <ramzi.hannachi1991@gmail.com>
2023-01-25 14:29:16 +0100

    update architecture Pizza decorator

--- design-pattern/decorator-pizza/src/ingredients/cheese.ts
+++ design-pattern/decorator-pizza/src/ingredients/cheese.ts
@@ -1,7 +1,11 @@
 import {PizzaDecorator} from "../decorator/pizza.js";
+import {Pizza} from "../pizza.js";
 
 export class CheeseDecorator extends PizzaDecorator {
     price = 2;
+    constructor(pizza: Pizza) {
+        super(pizza)
+    }
     getPrice(): number {
         return this.pizza.getPrice() + this.price;
     }
--- design-pattern/decorator-pizza/src/ingredients/pepperoni.ts
+++ design-pattern/decorator-pizza/src/ingredients/pepperoni.ts
@@ -1,7 +1,12 @@
 import {PizzaDecorator} from "../decorator/pizza.js";
+import {Pizza} from "../pizza.js";
 
 export class PepperoniDecorator extends PizzaDecorator {
     price = 3;
+
+    constructor(pizza: Pizza) {
+        super(pizza)
+    }
     getPrice(): number {
         return this.pizza.getPrice() + this.price;
     }
